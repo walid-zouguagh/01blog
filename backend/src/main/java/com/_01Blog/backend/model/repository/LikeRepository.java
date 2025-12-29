0ca696e9de78c091824e56a163b6708804f6c266
walid-zouguagh <walid.zouguaagh@gmail.com>
2025-12-29 12:31:34 +0100

    Create Likes the posts

--- backend/src/main/java/com/_01Blog/backend/model/repository/LikeRepository.java
+++ backend/src/main/java/com/_01Blog/backend/model/repository/LikeRepository.java
@@ -3,11 +3,28 @@ package com._01Blog.backend.model.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com._01Blog.backend.model.entity.Like;
 
 @Repository
-public interface LikeRepository extends JpaRepository<Like, UUID>{
+public interface LikeRepository extends JpaRepository<Like, UUID> {
 
+    @Query("""
+            SELECT EXISTS(SELECT 1 FROM Like l
+            WHERE l.postId = :postId AND l.userId = :userId)
+            """)
+    boolean isLiked(@Param("userId") UUID userId, @Param("postId") UUID postId);
+
+    @Query("""
+            DELETE FROM Like l WHERE l.userId = :userId AND l.postId = :postId
+            """)
+    void deleteLikeByPost(@Param("userId") UUID userId, @Param("postId") UUID postId);
+
+    @Query("""
+            SELECT DISTINCT COUNT(*) FROM Like l WHERE l.postId = :postId
+            """)
+    int countOfLikedPost(@Param("postId") UUID postId);
 }
--- backend/src/main/java/com/_01Blog/backend/service/LikeService.java
+++ backend/src/main/java/com/_01Blog/backend/service/LikeService.java
@@ -7,30 +7,46 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com._01Blog.backend.exception.ExceptionProgram;
+import com._01Blog.backend.model.entity.Like;
 import com._01Blog.backend.model.entity.Post;
 import com._01Blog.backend.model.entity.User;
 import com._01Blog.backend.model.repository.LikeRepository;
 import com._01Blog.backend.model.repository.PostRepository;
 
-// @Service
-// public class LikeService {
-//     private final LikeRepository likeRepository;
-//     private final PostRepository postRepository;
+@Service
+public class LikeService {
+    private LikeRepository likeRepository;
+    private PostRepository postRepository;
 
-//     @Transactional
-//     public Map<String, Object> like(User user, UUID postId) throws ExceptionProgram {
-//         if (postId == null) {
-//             throw new ExceptionProgram(400, "post not found");               
-//         }
-//         Post post = postRepository.findById(postId)
-//                     .orElseThrow(() -> new ExceptionProgram(400, "this post not found"));
+    @Transactional
+    public Map<String, Object> like(User user, UUID postId) throws ExceptionProgram {
+        if (postId == null) {
+            throw new ExceptionProgram(400, "post not found");
+        }
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ExceptionProgram(400, "this post not found"));
 
-//         if (post.isHidden()){
-//             new ExceptionProgram(400, "You can’t like this post");
-//         }
+        if (post.isHidden()) {
+            new ExceptionProgram(400, "You can’t like this post");
+        }
 
-//         // verify is liked 
-//         boolean isLiked = likeRepository.isLiked(user.getId(), postId);
-//     }
+        // verify is liked
+        boolean isLiked = likeRepository.isLiked(user.getId(), postId);
+        if (isLiked) {
+            // delete like
+            likeRepository.deleteLikeByPost(user.getId(), postId);
+        } else {
+            // save
+            Like like = new Like();
+            like.setPostId(post);
+            like.setUserId(user);
+            likeRepository.save(like);
+        }
 
-// }
+        int countLikedPost = likeRepository.countOfLikedPost(postId);
+        return Map.of(
+                "liked", !isLiked,
+                "countOfLikes", countLikedPost);
+    }
+
+}
